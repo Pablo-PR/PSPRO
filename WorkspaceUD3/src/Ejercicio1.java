907c4fcd1d2be9a56c8cfdaf7a2c34f3929a68b4
Pablo-PR <pabloparrarodriguez@gmail.com>
2022-01-25 14:34:52 +0100

    25-01

--- WorkspaceUD3/src/Ejercicio1.java
+++ WorkspaceUD3/src/Ejercicio1.java
@@ -15,35 +15,31 @@ public class Ejercicio1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("===========================================");
+		System.out.println("SALIDA PARA LOCALHOST: ");
 
-		{
-			
-			System.out.println("===========================================");
-			System.out.println("SALIDA PARA LOCALHOST: ");
-			try {
-				String nombreMaquinaLocal = InetAddress.getLocalHost().getHostName();
-				InetAddress dir = InetAddress.getByName(nombreMaquinaLocal);
-				
-				// LOCALHOST
-				dir = InetAddress.getByName(nombreMaquinaLocal);
-				pruebaMetodos(dir);//
-				// URL www.google.es
-				System.out.println("==========================================");
-				System.out.println("SALIDA PARA UNA URL:");
-				dir = InetAddress.getByName("www.google.es");
-				pruebaMetodos(dir);
-				// Array de tipo InetAddress con todas las direcciones IP
-				// asignadas a google.es
-				System.out.println("\tDIRECCIONES IP PARA: " + dir.getHostName());
-				InetAddress[] direcciones = InetAddress.getAllByName(dir.getHostName());
-				for (int i = 0; i < direcciones.length; i++)
-					System.out.println("\t\t" + direcciones[i].toString());
-				System.out.println("==========================================");
-			} catch (UnknownHostException el) {
-				el.printStackTrace();
-			}
-		} // main
+		try {
+			String nombreMaquinaLocal = InetAddress.getLocalHost().getHostName();
+			InetAddress dir = InetAddress.getByName(nombreMaquinaLocal);
+
+			// LOCALHOST
+			dir = InetAddress.getByName(nombreMaquinaLocal);
+			pruebaMetodos(dir);//
+			// URL www.google.es
+			System.out.println("==========================================");
+			System.out.println("SALIDA PARA UNA URL:");
+			dir = InetAddress.getByName("www.google.es");
+			pruebaMetodos(dir);
+			// Array de tipo InetAddress con todas las direcciones IP
+			// asignadas a google.es
+			System.out.println("\tDIRECCIONES IP PARA: " + dir.getHostName());
+			InetAddress[] direcciones = InetAddress.getAllByName(dir.getHostName());
+			for (int i = 0; i < direcciones.length; i++)
+				System.out.println("\t\t" + direcciones[i].toString());
+			System.out.println("==========================================");
+		} catch (UnknownHostException el) {
+			el.printStackTrace();
+		}
 
 	}
 
---
+++ WorkspaceUD3/src/Ejercicio7.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        String linea;
+
+        try {
+            URL url = new URL ("http://idocentic.website/servicios_para_explotar/obtener_ip.php");
+
+            InputStream in = url.openStream();
+            BufferedReader bf = new BufferedReader(new InputStreamReader(in));
+            linea = bf.readLine();
+
+            System.out.println(linea);
+
+            InetAddress dir = InetAddress.getByName(linea.trim());
+
+            System.out.println("\tMetodo getHostName(): " + dir.getHostName());
+            System.out.println("\tMetodo getHostAddress(): " + dir.getHostAddress());
+            System.out.println("\tMetodo getCanonicalHostName(): " + dir.getCanonicalHostName());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
